adf79906113d7a1f276ba247b833dc60ca12a33d
alexinea <forerunner@outlook.com>
2021-02-14 15:25:24 +0800

    Add ObjectContextVerifyiableAnnotation and StrongVerifiableAnnotation.

---
+++ src/Cosmos.Validation/Cosmos/Validation/Annotations/IObjectContextVerifiableAnnotation.cs
@@ -0,0 +1,17 @@
+﻿using Cosmos.Validation.Objects;
+
+namespace Cosmos.Validation.Annotations
+{
+    /// <summary>
+    /// A ObjectContext Verify Interface
+    /// </summary>
+    public interface IObjectContextVerifiableAnnotation : IVerifiable
+    {
+        /// <summary>
+        /// ObjectContext Verify
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        VerifyResult StrongVerify(ObjectContext context);
+    }
+}
---
+++ src/Cosmos.Validation/Cosmos/Validation/Annotations/IStrongVerifiableAnnotation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cosmos.Validation.Annotations
+{
+    /// <summary>
+    /// A Strong Verify Interface
+    /// </summary>
+    public interface IStrongVerifiableAnnotation : IVerifiable
+    {
+        /// <summary>
+        /// Strong Verify
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        VerifyResult StrongVerify<T>(T instance);
+
+        /// <summary>
+        /// Strong Verify
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        VerifyResult StrongVerify(Type type, object instance);
+    }
+
+    /// <summary>
+    /// A Strong Verify Interface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IStrongVerifiableAnnotation<in T> : IStrongVerifiableAnnotation
+    {
+        /// <summary>
+        /// Strong Verify
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        VerifyResult StrongVerify(T instance);
+    }
+}
