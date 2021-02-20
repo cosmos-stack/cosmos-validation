66a992708b1cd8aab3977704956f57dbcaba85bf
alexinea <forerunner@outlook.com>
2021-02-20 15:34:38 +0800

    Update ValidationHandler

--- src/Cosmos.Validation/Cosmos/Validation/ValidationHandler.cs
+++ src/Cosmos.Validation/Cosmos/Validation/ValidationHandler.cs
@@ -13,10 +13,10 @@ namespace Cosmos.Validation
 
         private readonly IValidationObjectResolver _objectResolver;
 
-        internal ValidationHandler(IEnumerable<IProject> projects,IValidationObjectResolver objectResolver)
+        internal ValidationHandler(IEnumerable<IProject> projects, IValidationObjectResolver objectResolver)
         {
             _objectResolver = objectResolver ?? throw new ArgumentNullException(nameof(objectResolver));
-            
+
             if (projects is not null)
                 foreach (var project in projects)
                     UpdateProject(project);
@@ -41,35 +41,117 @@ namespace Cosmos.Validation
                 }
             }
         }
-        
-        public VerifyResult Verify(Type type,object instance)
-        {
-            if (type is null)
-                throw new ArgumentNullException(nameof(type));
 
-            if (_typedProjects.TryGetValue(type.GetHashCode(), out var result))
-                return result.Verify(_objectResolver.Resolve(type, instance));
-            
+        #region Verify
+
+        public VerifyResult Verify(Type declaringType, object instance)
+        {
+            if (declaringType is null)
+                throw new ArgumentNullException(nameof(declaringType));
+
+            if (_typedProjects.TryGetValue(declaringType.GetHashCode(), out var result))
+                return result.Verify(_objectResolver.Resolve(declaringType, instance));
+
             return VerifyResult.UnexpectedType;
         }
 
-        public VerifyResult Verify(Type type,object instance,string name)
+        public VerifyResult Verify(Type declaringType, object instance, string projectName)
         {
-            if (string.IsNullOrWhiteSpace(name))
-                return Verify(type,instance);
-            
-            if (type is null)
-                throw new ArgumentNullException(nameof(type));
-            
-            if (_namedTypeProjects.TryGetValue((type.GetHashCode(), name.GetHashCode()), out var result))
-                return result.Verify(_objectResolver.Resolve(type, instance));
-            
+            if (string.IsNullOrWhiteSpace(projectName))
+                return Verify(declaringType, instance);
+
+            if (declaringType is null)
+                throw new ArgumentNullException(nameof(declaringType));
+
+            if (_namedTypeProjects.TryGetValue((declaringType.GetHashCode(), projectName.GetHashCode()), out var result))
+                return result.Verify(_objectResolver.Resolve(declaringType, instance));
+
             return VerifyResult.UnexpectedType;
         }
 
         public VerifyResult Verify<T>(T instance) => Verify(typeof(T), instance);
 
-        public VerifyResult Verify<T>(T instance, string name) => Verify(typeof(T), instance, name);
+        public VerifyResult Verify<T>(T instance, string projectName) => Verify(typeof(T), instance, projectName);
+
+        #endregion
+
+        #region VerifyOne
+
+        public VerifyResult VerifyOne(Type declaringType, Type memberType, object memberValue, string memberName)
+        {
+            if (declaringType is null)
+                throw new ArgumentNullException(nameof(declaringType));
+            
+            if (memberType is null)
+                throw new ArgumentNullException(nameof(memberType));
+
+            if (_typedProjects.TryGetValue(declaringType.GetHashCode(), out var result))
+            {
+                var valueContract = ObjectContractManager.Resolve(declaringType)?.GetValueContract(memberName);
+                var valueContext = ObjectValueContext.Create(memberValue, valueContract);
+                return result.VerifyOne(valueContext);
+            }
+
+            return VerifyResult.UnexpectedType;
+        }
+
+        public VerifyResult VerifyOne(Type declaringType, Type memberType, object memberValue, string memberName, string projectName)
+        {
+            if (string.IsNullOrWhiteSpace(projectName))
+                return VerifyOne(declaringType, memberType,memberValue,memberName);
+            
+            if (declaringType is null)
+                throw new ArgumentNullException(nameof(declaringType));
+            
+            if (memberType is null)
+                throw new ArgumentNullException(nameof(memberType));
+
+            if (_namedTypeProjects.TryGetValue((declaringType.GetHashCode(), projectName.GetHashCode()), out var result))
+            {
+                var valueContract = ObjectContractManager.Resolve(declaringType)?.GetValueContract(memberName);
+                var valueContext = ObjectValueContext.Create(memberValue, valueContract);
+                return result.VerifyOne(valueContext);
+            }
+
+            return VerifyResult.UnexpectedType;
+        }
+
+        public VerifyResult VerifyOne<TP, TM>(object memberValue, string memberName) 
+            => VerifyOne(typeof(TP), typeof(TM), memberValue, memberName);
+
+        public VerifyResult VerifyOne<TP, TM>(object memberValue, string memberName, string projectName) 
+            => VerifyOne(typeof(TP), typeof(TM), memberValue, memberName, projectName);
+        
+        #endregion
+
+        #region VerifyMany
+
+        public VerifyResult VerifyMany(Type declaringType, IDictionary<string, object> keyValueCollections)
+        {
+            if (declaringType is null)
+                throw new ArgumentNullException(nameof(declaringType));
+            
+            if (_typedProjects.TryGetValue(declaringType.GetHashCode(), out var result))
+                return result.Verify(_objectResolver.Resolve(declaringType, keyValueCollections));
+
+            return VerifyResult.UnexpectedType;
+        } 
+        
+        public VerifyResult VerifyMany(Type declaringType, IDictionary<string, object> keyValueCollections, string projectName)
+        {
+            if (string.IsNullOrWhiteSpace(projectName))
+                return VerifyMany(declaringType, keyValueCollections);
+
+            if (declaringType is null)
+                throw new ArgumentNullException(nameof(declaringType));
+
+            if (_namedTypeProjects.TryGetValue((declaringType.GetHashCode(), projectName.GetHashCode()), out var result))
+                return result.Verify(_objectResolver.Resolve(declaringType, keyValueCollections));
+
+            return VerifyResult.UnexpectedType;
+        } 
+
+        #endregion
 
         internal ValidationHandler Merge(IEnumerable<IProject> projects)
         {
