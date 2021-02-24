5129a4f4adc5bc30177b5149ad5aa6eaea294a14
alexinea <forerunner@outlook.com>
2021-02-24 21:34:24 +0800

    Add UT for strategy.

---
+++ tests/CosmosValidationUT/StrategyUT/GenericNiceBoatStrategy.cs
@@ -0,0 +1,15 @@
+﻿using Cosmos.Validation.Strategies;
+using CosmosValidationUT.Models;
+
+namespace CosmosValidationUT.StrategyUT
+{
+    public class GenericNiceBoatStrategy : ValidationStrategy<NiceBoat>
+    {
+        public GenericNiceBoatStrategy()
+        {
+            ForMember(x => x.Name).NotEmpty();
+            ForMember(x => x.Length).GreaterThanOrEqual(0);
+            ForMember(x => x.Width).GreaterThanOrEqual(0);
+        }
+    }
+}
---
+++ tests/CosmosValidationUT/StrategyUT/NormalNiceBoatStrategy.cs
@@ -0,0 +1,15 @@
+﻿using Cosmos.Validation.Strategies;
+using CosmosValidationUT.Models;
+
+namespace CosmosValidationUT.StrategyUT
+{
+    public class NormalNiceBoatStrategy : ValidationStrategy
+    {
+        public NormalNiceBoatStrategy() : base(typeof(NiceBoat))
+        {
+            ForMember("Name").NotEmpty();
+            ForMember("Length").GreaterThanOrEqual(0);
+            ForMember("Width").GreaterThanOrEqual(0);
+        }
+    }
+}
