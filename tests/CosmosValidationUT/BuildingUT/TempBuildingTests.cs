a5eac83f6e4470cb092b8c9792355819f9972b3a
alexinea <forerunner@outlook.com>
2021-03-08 10:46:19 +0800

    Add TempBuild UT.

---
+++ tests/CosmosValidationUT/BuildingUT/TempBuildingTests.cs
@@ -0,0 +1,33 @@
+﻿using Cosmos.Validation;
+using Cosmos.Validation.Registrars;
+using CosmosValidationUT.Models;
+using Shouldly;
+using Xunit;
+
+namespace CosmosValidationUT.BuildingUT
+{
+    [Trait("BuildingUT", "TempBuilding")]
+    public class TempBuildingTests
+    {
+        [Fact(DisplayName = "TempBuild test")]
+        public void TempBuildTest()
+        {
+            var model1 = new NiceBoat {Name = "Haha"};
+            var model2 = new NiceBoat {Name = ""};
+
+            var options = new ValidationOptions()
+            {
+                AnnotationEnabled = false,
+                CustomValidatorEnabled = false
+            };
+
+            var handler = ValidationRegistrar.DefaultRegistrar
+                                             .ForType<NiceBoat>().ForMember(x => x.Name).NotEmpty()
+                                             .TempBuild(options);
+
+            handler.ShouldNotBeNull();
+            handler.Verify(model1).IsValid.ShouldBeTrue();
+            handler.Verify(model2).IsValid.ShouldBeFalse();
+        }
+    }
+}
