8837312afa47d0ac7761f9ec4f01437ec2cade99
alexinea <forerunner@outlook.com>
2021-03-17 17:10:27 +0800

    Refactor Compare-Class tokens.

---
+++ src/Cosmos.Validation/Cosmos/Validation/Internals/Tokens/ValueTokens/Basic/ValueCompareBasicToken.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Cosmos.Validation.Objects;
+
+namespace Cosmos.Validation.Internals.Tokens.ValueTokens.Basic
+{
+    internal abstract class ValueCompareBasicToken : ValueToken
+    {
+        private readonly object _valueToCompare;
+        private readonly Type _typeOfValueToCompare;
+
+        protected ValueCompareBasicToken(VerifiableMemberContract contract, object valueToCompare, string tokenName) : base(contract)
+        {
+            _valueToCompare = valueToCompare;
+            _typeOfValueToCompare = _valueToCompare.GetType();
+            TokenName = tokenName;
+        }
+
+        public override string TokenName { get; }
+
+        public override bool MutuallyExclusive => false;
+
+        public override int[] MutuallyExclusiveFlags => NoMutuallyExclusiveFlags;
+
+        public override CorrectVerifyVal Valid(VerifiableObjectContext context)
+        {
+            var verifyVal = CreateVerifyVal();
+
+            var value = GetValueFrom(context);
+
+            if (!IsValidImpl(value, _valueToCompare, _typeOfValueToCompare, out var message))
+            {
+                UpdateVal(verifyVal, value, message);
+            }
+
+            return verifyVal;
+        }
+
+        public override CorrectVerifyVal Valid(VerifiableMemberContext context)
+        {
+            var verifyVal = CreateVerifyVal();
+
+            var value = GetValueFrom(context);
+
+            if (!IsValidImpl(value, _valueToCompare, _typeOfValueToCompare, out var message))
+            {
+                UpdateVal(verifyVal, value, message);
+            }
+
+            return verifyVal;
+        }
+
+        protected abstract bool IsValidImpl(object value, object valueToCompare, Type typeOfValueToCompare, out string message);
+
+        protected virtual void UpdateVal(CorrectVerifyVal val, object obj, object valueToCompare, string message = null)
+        {
+            val.IsSuccess = false;
+            val.VerifiedValue = obj;
+            val.ErrorMessage = MergeMessage(message ?? $"The given value does not meet the requirements.");
+        }
+
+        public override string ToString() => TokenName;
+    }
+}
--- src/Cosmos.Validation/Cosmos/Validation/Internals/Tokens/ValueTokens/ValueLessThanToken.cs
+++ src/Cosmos.Validation/Cosmos/Validation/Internals/Tokens/ValueTokens/ValueLessThanToken.cs
@@ -1,69 +1,29 @@
 ﻿using System;
+using Cosmos.Validation.Internals.Tokens.ValueTokens.Basic;
 using Cosmos.Validation.Objects;
 
 namespace Cosmos.Validation.Internals.Tokens.ValueTokens
 {
-    internal class ValueLessThanToken : ValueToken
+    internal class ValueLessThanToken : ValueCompareBasicToken
     {
         // ReSharper disable once InconsistentNaming
         public const string NAME = "ValueLessThanToken";
 
-        private readonly object _valueToCompare;
-        private readonly Type _typeOfValueToCompare;
+        public ValueLessThanToken(VerifiableMemberContract contract, object valueToCompare) : base(contract, valueToCompare, NAME) { }
 
-        public ValueLessThanToken(VerifiableMemberContract contract, object valueToCompare) : base(contract)
-        {
-            _valueToCompare = valueToCompare;
-            _typeOfValueToCompare = _valueToCompare.GetType();
-        }
-
-        public override string TokenName => NAME;
-
-        public override bool MutuallyExclusive => false;
-
-        public override int[] MutuallyExclusiveFlags => NoMutuallyExclusiveFlags;
-        
-        public override CorrectVerifyVal Valid(VerifiableObjectContext context)
-        {
-            var verifyVal = CreateVerifyVal();
-           
-            var value = GetValueFrom(context);
-
-            if (!IsValidImpl(value, out var message))
-            {
-                UpdateVal(verifyVal, value, message);
-            }
-
-            return verifyVal;
-        }
-
-        public override CorrectVerifyVal Valid(VerifiableMemberContext context)
-        {
-            var verifyVal = CreateVerifyVal();
-           
-            var value = GetValueFrom(context);
-
-            if (!IsValidImpl(value, out var message))
-            {
-                UpdateVal(verifyVal, value, message);
-            }
-
-            return verifyVal;
-        }
-
-        private bool IsValidImpl(object value, out string message)
+        protected override bool IsValidImpl(object value, object valueToCompare, Type typeOfValueToCompare, out string message)
         {
             message = null;
 
-            if (VerifiableMember.MemberType.IsValueType && _typeOfValueToCompare.IsValueType)
+            if (VerifiableMember.MemberType.IsValueType && typeOfValueToCompare.IsValueType)
             {
-                if (ValueTypeEqualCalculator.CompareTo(VerifiableMember.MemberType, value, _typeOfValueToCompare, _valueToCompare) < 0)
+                if (ValueTypeEqualCalculator.CompareTo(VerifiableMember.MemberType, value, typeOfValueToCompare, valueToCompare) < 0)
                 {
                     return true;
                 }
             }
 
-            else if (value is IComparable c1 && _valueToCompare is IComparable c2)
+            else if (value is IComparable c1 && valueToCompare is IComparable c2)
             {
                 if (c1.CompareTo(c2) < 0)
                     return true;
@@ -73,13 +33,11 @@ namespace Cosmos.Validation.Internals.Tokens.ValueTokens
             return false;
         }
 
-        private void UpdateVal(CorrectVerifyVal val, object obj, string message = null)
+        protected override void UpdateVal(CorrectVerifyVal val, object obj, object valueToCompare, string message = null)
         {
             val.IsSuccess = false;
             val.VerifiedValue = obj;
-            val.ErrorMessage = MergeMessage(message ?? $"The given value must be less than {_valueToCompare}.");
+            val.ErrorMessage = MergeMessage(message ?? $"The given value must be less than {valueToCompare}.");
         }
-
-        public override string ToString() => NAME;
     }
 }
