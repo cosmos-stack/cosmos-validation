64fc54cada182c6689117e4df360f8e198e97d55
alexinea <forerunner@outlook.com>
2021-04-09 15:30:47 +0800

    Add Autofac extensions for Cosmos validation dependency

---
+++ src/Cosmos.Validation.Extensions.Autofac/Autofac/ValidationRegisterExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Cosmos.Dependency;
+
+namespace Autofac
+{
+    public static class ValidationRegisterExtensions
+    {
+        public static ContainerBuilder RegisterCosmosValidation(this ContainerBuilder builder)
+        {
+            using var register = new AutofacProxyRegister(builder);
+            register.RegisterValidation();
+
+            return builder;
+        }
+    }
+}
