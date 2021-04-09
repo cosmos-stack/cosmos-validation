94aa60d3b15326191ee955447abc6a40af602957
alexinea <forerunner@outlook.com>
2021-04-09 15:30:31 +0800

    Add NCC AspectCore-Framework for Cosmos validation dependency

---
+++ src/Cosmos.Validation.Extensions.AspectCoreInjector/AspectCore/DependencyInjection/ValidationRegisterExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Cosmos.Dependency;
+
+namespace AspectCore.DependencyInjection
+{
+    public static class ValidationRegisterExtensions
+    {
+        public static IServiceContext AddCosmosValidation(this IServiceContext context)
+        {
+            using var register = new AspectCoreProxyRegister(context);
+            register.RegisterValidation();
+
+            return context;
+        }
+    }
+}
