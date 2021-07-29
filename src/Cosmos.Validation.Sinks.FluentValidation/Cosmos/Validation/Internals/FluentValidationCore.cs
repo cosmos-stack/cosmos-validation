b871eed9ec9ff55ea0899f3590b092f3405c79bd
AlexLEWIS <liuyis@live.com>
2021-07-29 11:35:35 +0800

    Optimize dynamic scripts and cache Func generated by Natasha.

--- src/Cosmos.Validation.Sinks.FluentValidation/Cosmos/Validation/Internals/FluentValidationCore.cs
+++ src/Cosmos.Validation.Sinks.FluentValidation/Cosmos/Validation/Internals/FluentValidationCore.cs
@@ -1,22 +1,41 @@
-﻿using System;
+﻿#if NET452
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Cosmos.Collections;
-using Cosmos.Reflection;
-using Cosmos.Validation.Internals.Extensions;
 using Cosmos.Validation.Objects;
 using FluentValidation;
 using FluentValidation.Internal;
 using FluentValidation.Results;
 using FluentValidation.Validators;
 
+#elif NETFRAMEWORK
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cosmos.Collections;
+using Cosmos.Validation.Objects;
+using FluentValidation;
+using FluentValidation.Internal;
+using FluentValidation.Results;
+using FluentValidation.Validators;
+
+#else
+using System;
+using System.Collections.Concurrent;
+using Cosmos.Collections;
+using Cosmos.Validation.Internals.Extensions;
+using Cosmos.Validation.Objects;
+using FluentValidation;
+using FluentValidation.Results;
+
+#endif
+
 namespace Cosmos.Validation.Internals
 {
     internal static class FluentValidationCore
     {
 #if !NETFRAMEWORK
-
         static FluentValidationCore()
         {
             NatashaInitializer.InitializeAndPreheating();
@@ -91,45 +110,60 @@ namespace Cosmos.Validation.Internals
         {
             var propertyValidators = validator.CreateDescriptor().GetValidatorsForMember(context.MemberName);
             var parentContext = FluentValidationContextFactory.Resolve(context.DeclaringType, default);
-            
+
             var originalFailures = new List<ValidationFailure>();
             var nameOfExecutedRules = new List<string>();
-            
+
             foreach (var propertyValidator in propertyValidators)
             {
-                foreach (var rule in (IEnumerable<IValidationRule>) validator)
+                foreach (var rule in (IEnumerable<IValidationRule>)validator)
                 {
                     if (rule is PropertyRule propertyRule)
                     {
                         var propertyValidatorContext = new PropertyValidatorContext(parentContext, propertyRule, context.MemberName, context.Value);
-            
+
                         var localFailures = propertyValidator.Validate(propertyValidatorContext);
-            
+
                         originalFailures.AddRange(localFailures);
                         nameOfExecutedRules.AddRange(propertyRule.RuleSets);
                     }
                 }
             }
-            
+
             if (!originalFailures.Any())
                 return VerifyResult.Success;
-            
+
             var failures = originalFailures.ConvertToVerifyFailures(typeOfValidator);
-            return new VerifyResult(failures) {NameOfExecutedRules = nameOfExecutedRules.Distinct().ToArray()};
-            
-            // Validation/Sink-for-FluentValidation will no-longer support VerifyOne any more.
-            // return VerifyResult.Success;
+            return new VerifyResult(failures) { NameOfExecutedRules = nameOfExecutedRules.Distinct().ToArray() };
         }
 #else
+        private static readonly ConcurrentDictionary<(Type, Type, string), Func<IValidator, VerifiableMemberContext, ValidationResult>> _dynamicFuncCache = new();
+
         public static VerifyResult VerifyOne(IValidator validator, VerifiableMemberContext context, Type typeOfValidator)
         {
             if (!validator.CanValidateInstancesOfType(context.DeclaringType))
                 return VerifyResult.Success;
 
-            var friendlyNameOfValidationResult = typeof(ValidationResult).GetFriendlyName();
-            var friendlyNameOfDeclaringType = context.DeclaringType.GetFriendlyName();
+            var key = (typeOfValidator, context.DeclaringType, context.MemberName);
 
-            var script = @$"
+            var func = _dynamicFuncCache.GetOrAdd(key, k => __dynamicFuncFactory(k.Item2, k.Item3));
+
+            var v = func.Invoke(validator, context);
+
+            if (v.IsValid)
+                return VerifyResult.Success;
+
+            var failures = v.Errors.ConvertToVerifyFailures(typeOfValidator);
+
+            return new VerifyResult(failures) { NameOfExecutedRules = v.RuleSetsExecuted };
+
+            // ReSharper disable once InconsistentNaming
+            Func<IValidator, VerifiableMemberContext, ValidationResult> __dynamicFuncFactory(Type declaringType, string memberName)
+            {
+                var friendlyNameOfValidationResult = typeof(ValidationResult).GetFriendlyName();
+                var friendlyNameOfDeclaringType = declaringType.GetFriendlyName();
+
+                var script = @$"
 if(arg1 is not FluentValidation.IValidator<{friendlyNameOfDeclaringType}> realValidator)
     return new {friendlyNameOfValidationResult}();
 
@@ -138,24 +172,15 @@ if(@try.IsFailure)
     return new {friendlyNameOfValidationResult}();
 
 var instance = @try.Value;
-instance.{context.MemberName} = ({context.MemberType.GetFriendlyName()})arg2.Value;
+instance.{memberName} = ({context.MemberType.GetFriendlyName()})arg2.Value;
 
 return FluentValidation.DefaultValidatorExtensions.Validate<{friendlyNameOfDeclaringType}>(
     realValidator,
     instance,
     options => options.IncludeProperties(arg2.MemberName)
 );";
-
-            var v = NDelegate.RandomDomain()
-                             .Func<IValidator, VerifiableMemberContext, ValidationResult>(script)
-                             .Invoke(validator, context);
-            
-            if (v.IsValid)
-                return VerifyResult.Success;
-
-            var failures = v.Errors.ConvertToVerifyFailures(typeOfValidator);
-
-            return new VerifyResult(failures) { NameOfExecutedRules = v.RuleSetsExecuted };
+                return NDelegate.RandomDomain().Func<IValidator, VerifiableMemberContext, ValidationResult>(script);
+            }
         }
 #endif
     }
