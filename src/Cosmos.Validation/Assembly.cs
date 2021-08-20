1605fb5b01c1ece32320f9b989c8ee27a9deb55c
AlexLEWIS <liuyis@live.com>
2021-08-20 14:30:07 +0800

    Update Assembly.cs

--- src/Cosmos.Validation/Assembly.cs
+++ src/Cosmos.Validation/Assembly.cs
@@ -16,7 +16,6 @@
 //Cosmos Validation Sinks
 [assembly: InternalsVisibleTo("Cosmos.Validation.Sinks.DataAnnotations")]
 [assembly: InternalsVisibleTo("Cosmos.Validation.Sinks.FluentValidation")]
-[assembly: InternalsVisibleTo("Cosmos.Validation.Sinks.SwallowValidation")]
 
 //Cosmos Prowess Package
 [assembly: InternalsVisibleTo("Cosmos.Extensions.ObjectVisitors")]
